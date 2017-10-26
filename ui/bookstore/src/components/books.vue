7ad6e4a0266315fc08f85526b8825155227acec1
Lucas Lacerda <lucasalvlacerda@gmail.com>
2017-10-26 01:54:59 -0200

    Add new book card component on the ui

--- ui/bookstore/src/components/books.vue
+++ ui/bookstore/src/components/books.vue
@@ -1,17 +1,13 @@
 <template>
-    <div>
-        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <div class="w3-row-padding">
-            <div class="w3-col s4 m6 l6">
-                <div class="w3-card-4 w3-center" >
-                    <img :src="image_url" alt="book-image"  style="width:100%">
-                        <h5>{{title}}</h5>
-                        <p>{{description}}</p>
+        <div class="w3-container w3-row-padding">
+            <div class="w3-card-4 w3-center w3-hover-shadow w3-col m4 l1 w3-mobile">
+                <img :src="image_url" alt="Norway" style="width:100%">
+                <div class="w3-center">
+                    <h5 style="font-size: 10px">{{title}}</h5>
+                    <p style="font-size: 10px">{{description}}</p>
                 </div>
             </div>
         </div>
-    </div>
 </template>
 
 <script>
@@ -20,23 +16,3 @@ export default {
   props: ['title', 'image_url', 'description']
 };
 </script>
-
-<style>
-
-    p{
-        font-size: 10px;
-    }
-
-    .w3-card-4{
-        width: 250px;
-        height: 300px;
-        padding: 0.2em 0.2em;
-        position: center;
-    }
-    img{
-        width: 100%;
-        height: 200px;
-    }
-
-</style>
-
