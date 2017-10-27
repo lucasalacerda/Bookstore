21db6c43edeb83f76ecfb604c4da95dc77f285ff
Lucas Lacerda <lucasalvlacerda@gmail.com>
2017-10-27 01:06:58 -0200

    Add some changes on the header layout. Like the background-image

--- ui/bookstore/src/components/header.vue
+++ ui/bookstore/src/components/header.vue
@@ -4,16 +4,15 @@
 
     <div class="w3-bar w3-light-grey">
         <div class="w3-display-container">
-            <div class="w3-bar" style="max-height: 200px">
-                <img src="../img/books.png" class="w3-image w3-opacity-min  w3-grayscale-min" style="min-width:100%; min-height: auto;">
-                <div class="w3-display-middle w3-container w3-wide w3-mobile"
+            <div class="w3-bar img-bar">
+                <div class="title-bookstore w3-display-middle w3-container w3-wide w3-mobile"
                 style="font-size: 60px; text-shadow:1px 1px 0 rgb(51, 47, 47)">Bookstore</div>
             </div>
         </div>
         <div class="links">
             <a href="#" class="w3-bar-item w3-btn w3-wide w3-mobile">Home</a>
-            <a href="#" class="w3-bar-item w3-btn w3-wide w3-mobile">Link 1</a>
-            <a href="#" class="w3-bar-item w3-btn w3-wide w3-mobile">Link 2</a>
+            <a href="#" class="w3-bar-item w3-btn w3-wide w3-mobile">Admin</a>
+            <a href="#" class="w3-bar-item w3-btn w3-wide w3-mobile">Contato</a>
             <a href="#" class="w3-bar-item w3-button w3-grey w3-right w3-wide w3-mobile">Go!</a>
             <input type="text" class="w3-bar-item w3-input w3-right w3-mobile" placeholder="Search..">
         </div>
@@ -26,6 +25,28 @@ export default {
   name: "headerComponent"
 };
 </script>
+
+<<style>
+    .img-bar{
+        height: 250px;
+        background: url('../img/books.png') no-repeat 50%;
+        background-size: cover;
+        background-attachment: fixed;
+    }
+    .img-bar:before{
+        height: 100%;
+        width: 100%;
+        background: #000;
+        opacity: 0.4;
+    }
+
+    .title-bookstore{
+        font-size: 60px;
+        text-shadow: 0px 0px 50px rgb(51, 47, 47);
+        color: #fff;
+    }
+</style>
+
 
 
 
