a269da81dd6708ba7c0a02ea517c8fff45903f82
Lucas Lacerda <lucasalvlacerda@gmail.com>
2017-10-27 01:05:49 -0200

    add some fixes on the card component

--- ui/bookstore/src/components/books.vue
+++ ui/bookstore/src/components/books.vue
@@ -1,18 +1,41 @@
 <template>
-        <div class="w3-container w3-row-padding">
-            <div class="w3-card-4 w3-center w3-hover-shadow w3-col m4 l1 w3-mobile">
-                <img :src="image_url" alt="Norway" style="width:100%">
-                <div class="w3-center">
-                    <h5 style="font-size: 10px">{{title}}</h5>
-                    <p style="font-size: 10px">{{description}}</p>
-                </div>
-            </div>
-        </div>
+            <li class="card-book-item w3-card-4 w3-center w3-col m4 l2 w3-mobile">
+                <div clas="image">
+                    <img :src="image_url">
+                </div>   
+                    <a href="#" style="font-size: 10px">
+                        <p>{{title}}</p>
+                    </a>
+            </li>
 </template>
 
 <script>
 export default {
-  name: "booksComponent",
-  props: ['title', 'image_url', 'description']
+  name: "booksCardComponent",
+  props: ["title", "image_url", "description"]
 };
 </script>
+
+
+<style>
+    img{
+        max-height: 130px;
+        width: 130px;
+        margin-top: 5px
+    }
+
+    .card-book-item{
+        height: 180px;
+        list-style-type: none;
+        display: inline-block;
+        position: relative;
+        margin-right: 10px;
+        margin-bottom: 30px;
+    }
+    p{
+        width: 190px; /* Tamanho */
+        overflow: hidden;
+        text-overflow: ellipsis;
+        white-space: nowrap
+    }
+</style>
