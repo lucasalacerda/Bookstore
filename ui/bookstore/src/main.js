17ebccfba02e8cdc72eac7d575986dda2d2011e5
Lucas Lacerda <lucasalvlacerda@gmail.com>
2017-10-27 01:29:33 -0200

    Add routes on the main.js

--- ui/bookstore/src/main.js
+++ ui/bookstore/src/main.js
@@ -1,13 +1,32 @@
 import Vue from 'vue'
-import App from './home.vue'
 import VueResource from 'vue-resource'
 Vue.use(VueResource);
 
+//import pages
+import App from './home.vue'
+import SearchResult from './pages/searchResult.vue'; 
+import AdminPage from './pages/adminPage.vue'
 
 import cors from 'cors';
+
+const NotFound = { template: '<p>Página não encontrada</p>' }
+
+const routes = {
+  '/': App,
+  '/results': SearchResult,
+  '/admin': AdminPage,
+}
 
 
 new Vue({
   el: '#app',
-  render: h => h(App)
+  data: {
+    currentRoute: window.location.pathname
+  },
+  computed: {
+    ViewComponent () {
+      return routes[this.currentRoute] || NotFound
+    }
+  },
+  render (h) { return h(this.ViewComponent) }
 })
