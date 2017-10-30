b78dc6f2f4e0948f642ec2917fbfe4184cd91a89
Lucas Lacerda <lucasalvlacerda@gmail.com>
2017-10-29 23:33:45 -0200

    Add table components on the adminPage

--- ui/bookstore/src/pages/adminPage.vue
+++ ui/bookstore/src/pages/adminPage.vue
@@ -1,8 +1,22 @@
 <template>
   <div id="contain">
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-
+    
     <header-component></header-component>
-   
+    <book-list-component></book-list-component>
+
   </div>
 </template>
+
+<script>
+
+import headerComponent from '../components/header.vue';
+import bookListComponent from '../components/booksList.vue';
+
+export default {
+  components: {
+    headerComponent,
+    bookListComponent,
+    },
+};
+</script>
