cbd1b56b4879e0939e13fe9ae3a32559464ca791
Lucas Lacerda <lucasalvlacerda@gmail.com>
2017-10-29 23:07:40 -0200

    Add a slide on the homepage but it's doesn't work yet

--- ui/bookstore/src/home.vue
+++ ui/bookstore/src/home.vue
@@ -3,9 +3,13 @@
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 
     <header-component></header-component>
-    <ul class="w3-container w3-row-padding">
+        <div class="slide_show w3-display-container">
+            <button class="w3-button w3-black	w3-display-left" onclick="plusDivs(-1)">&#10094;</button>
+            <button class="w3-button w3-black w3-display-right" onclick="plusDivs(1)">&#10095;</button>
+        </div>
+    <ul class="w3-container w3-row-padding">      
       <books-card-component v-bind:key="book.id" v-for="book in books"
-      :title="book.title" :description="book.description" :image_url="book.image_url"></books-card-component>
+      :title="book.title" :image_url="book.image_url"></books-card-component>
     </ul>
   </div>
 </template>
@@ -57,10 +61,23 @@ h1{
 }
 
 body{
-  background-size: cover;
-  background-repeat: no-repeat; 
   margin: 0px;
+  padding: 0px;
 }
+
+.slide_show{
+  height: 250px;
+  background: url('./img/promo_livro.jpg') no-repeat 50% 50%;
+  background-size: cover;
+  box-shadow: 0.1cm 0.1ch 1ch;
+}
+
+.slide_show:before{
+        height: 100%;
+        width: 100%;
+        background: #000;
+        opacity: 0.4;
+    }
 
 @media only screen and (max-device-width: 1024px) {
    
