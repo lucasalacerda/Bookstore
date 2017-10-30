f6f790a2637c05904f65bcbed8e7f3ebebc98458
Lucas Lacerda <lucasalvlacerda@gmail.com>
2017-10-30 01:41:49 -0200

    Add a new modal component for the add books

---
+++ ui/bookstore/src/components/modalAddBook.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="w3-right">
+        <button id="addBook" v-on:click="openModal" class="add-button w3-btn w3-wide" style="margin: 0.7em 0em">Add Books</button>
+        <div id="modalForm" class="w3-modal">
+            <div class="w3-modal-content w3-card-4 w3-animate-zoom" style="max-width:600px">
+                <div class="w3-center"><br>
+                    <span v-on:click="closeModal" class="w3-button w3-xlarge w3-hover-red w3-display-topright" title="Close Modal">&times;</span>
+                </div>
+            <form class="w3-container" action="/action_page.php">
+                <div class="w3-section">
+                    <h2 class="w3-left">Add Book</h2>
+                    <input class="w3-input w3-margin-bottom" type="text" placeholder="Title" name="book_title" required>
+                    <input class="w3-input w3-margin-bottom" type="text" placeholder="Genre" name="book_genre" required>
+                    <input class="w3-input  w3-margin-bottom" type="textarea" placeholder="Description" name="book_description" required>
+                    <input class="w3-input  w3-margin-bottom" type="text" placeholder="Author" name="book_author" required>
+                    <input class="w3-input  w3-margin-bottom" type="text" placeholder="Publisher" name="book_publisher" required>
+                    <input class="w3-input  w3-margin-bottom" type="text" placeholder="Number of pages" name="book_pages" required>
+                    <button class="w3-button w3-block w3-green w3-section w3-padding" type="submit">Add</button>
+                </div>
+            </form>
+            </div>
+        </div>
+    </div>
+</template>           
+
+<script>
+
+export default {
+    name:'modalAddBookComponent',
+    methods:{
+      openModal: function(){
+          document.getElementById('modalForm').style.display='block';
+      },
+      closeModal: function(){
+        document.getElementById('modalForm').style.display='none';
+      }
+    },
+    beforeCreate() {
+      console.log('beforeCreated');
+    },
+    created() {
+         this.$http.get('http://localhost:3000/api/books').then(response => {
+          this.books = response.body;
+    }, function(response){
+      console.log(response.data);
+      }
+    )},
+    beforeMount(){
+      console.log('beforeMount');
+    },
+
+    data(){
+      return{
+        books: [],
+      }
+    }
+}   
+</script>
